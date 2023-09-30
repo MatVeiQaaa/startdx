5b9e559dc7b04eb1df9c2d6c7cd2dc7b49d8616e
mtvqaaa <matthehard@gmail.com>
2023-09-30 00:00:32 -0700

    Pattern scan for everything

--- src/startdx/startdx.cpp
+++ src/startdx/startdx.cpp
@@ -3,10 +3,12 @@
 #include <windows.h>
 #include <iostream>
 #include <thread>
+#include <psapi.h>
 
 #include "helpers.hpp"
 
 #include "minhook/include/MinHook.h"
+#include "sigmatch/include/sigmatch/sigmatch.hpp"
 
 #if defined _M_X64
 #pragma comment(lib, "libMinHook.x64.lib")
@@ -62,15 +64,6 @@ int __fastcall OnGetDarkLaneSafe(DWORD* pThis, DWORD* edxArg)
 float* darkLaneOptMaxIdx = NULL;
 int* darkLaneOptIdxCount = NULL;
 
-//// Pointer for calling original MessageBoxW.
-//MESSAGEBOXW fpMessageBoxW = NULL;
-//
-//// Detour function which overrides MessageBoxW.
-//int WINAPI DetourMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType)
-//{
-//	return fpMessageBoxW(hWnd, L"Hooked!", lpCaption, uType);
-//}
-
 void SetVariables()
 {
 	while (!CanAccess((uintptr_t)darkLaneOptIdxCount) || !CanAccess((uintptr_t)darkLaneOptMaxIdx))
@@ -86,16 +79,26 @@ void SetVariables()
 
 int startdx::init(HMODULE hModule)
 {
-	uintptr_t modBm2dx = (uintptr_t)GetModuleHandle("bm2dx.dll");
+	Sleep(10000);
+	uintptr_t hBm2dx = (uintptr_t)GetModuleHandle("bm2dx.dll");
+	MODULEINFO modBm2dx;
+	GetModuleInformation(GetCurrentProcess(), (HMODULE)hBm2dx, &modBm2dx, sizeof(MODULEINFO));
 
-	GetPlayer = (getPlayer)(modBm2dx + 0x913220);
-	GetSide = (getSide)(modBm2dx + 0x83C390);
-	SetDarkLane = (setDarkLane)(modBm2dx + 0x9449F0);
-	GetDarkLane = (getDarkLane)(modBm2dx + 0x93FAA0);
 
-	darkLaneOptMaxIdx = (float*)(modBm2dx + 0x12FDD00);
-	darkLaneOptIdxCount = (int*)(modBm2dx + 0x1185758);
+	using namespace sigmatch_literals;
+	sigmatch::this_process_target target;
+	sigmatch::search_context context = target.in_module("bm2dx.dll");
 
+	GetPlayer = (getPlayer)(context.search("B8 08 00 00 00 48 6B C0 01 48 8D 0D ?? ?? ?? ?? 48 8B 04 01 C3"_sig).matches().front());
+	GetSide = (getSide)(context.search("48 83 EC 38 E8 ?? ?? ?? ?? 85 C0 74 0A C7 44 24 20 01 00 00 00 EB 08 C7 44 24 20 00 00 00 00 8B 44 24 20 48 83 C4 38 C3"_sig).matches().front());
+	SetDarkLane = (setDarkLane)(context.search("E8 ?? ?? ?? ?? 8B 4C 24 40 89 88 8C 00 00 00"_sig).matches().front() - 0x25);
+	GetDarkLane = (getDarkLane)(context.search("E8 ?? ?? ?? ?? 8B 80 8C 00 00 00 48 83 C4 28"_sig).matches().front() - 0x20);
+	SetDarkLaneSafe = (setDarkLaneSafe)(context.search("8B 84 24 90 00 00 00 89 44 24 20 83 7C 24 20 05"_sig).matches().front() - 0x12);
+	GetDarkLaneSafe = (getDarkLaneSafe)(context.search("48 03 C1 FF E0 33 C0 EB 25 B8 01 00 00 00 EB 1E B8 02 00 00 00 EB 17"_sig).matches().front() - 0x51);
+	DWORD* maxIdxOffset = (DWORD*)(context.search("F3 0F 5E 05 ?? ?? ?? ?? F3 0F 2C C0 89 84 24 B8 00 00 00"_sig).matches().front() + 0x04);
+	darkLaneOptMaxIdx = (float*)((long long)maxIdxOffset + *maxIdxOffset + sizeof(float));
+	darkLaneOptIdxCount = (int*)(context.search("06 00 00 00 00 00 00 00 01 00 00 00 02 00 00 00 04 00 00 00 08 00 00 00 10 00 00 00 20"_sig).matches().front());
+	
 	std::thread setVariables(SetVariables);
 	setVariables.detach();
 
@@ -104,12 +107,12 @@ int startdx::init(HMODULE hModule)
 		return 1;
 	}
 
-	if (MH_CreateHookEx((LPVOID)(modBm2dx + 0x7F2130), &OnSetDarkLaneSafe, &SetDarkLaneSafe) != MH_OK)
+	if (MH_CreateHookEx((LPVOID)SetDarkLaneSafe, &OnSetDarkLaneSafe, &SetDarkLaneSafe) != MH_OK)
 	{
 		return 1;
 	}
 
-	if (MH_CreateHookEx((LPVOID)(modBm2dx + 0x7F0A90), &OnGetDarkLaneSafe, &GetDarkLaneSafe) != MH_OK)
+	if (MH_CreateHookEx((LPVOID)GetDarkLaneSafe, &OnGetDarkLaneSafe, &GetDarkLaneSafe) != MH_OK)
 	{
 		return 1;
 	}
